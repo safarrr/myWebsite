25db27b6de9982a6be6bb0243308065e8a09eefa
safarrr <safar190305@gmail.com>
2023-12-24 11:46:52 +0700

    add dark mode 🌚

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
@@ -9,5 +10,5 @@ module.exports = {
       },
     },
   },
-  plugins: [ require('@tailwindcss/typography'),],
+  plugins: [require("@tailwindcss/typography")],
 };
